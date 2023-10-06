8175e847d37218664080b61bf7b0fddf5830192f
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-06 09:18:11 +0000

    test(cstr): added tests for cstr_alloc

--- src/cstr/cstr_alloc.c
+++ src/cstr/cstr_alloc.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 11:53:27 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/05 11:54:20 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/06 09:16:40 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@
 
 #include "safe_alloc.h"
 
+/**
+ * @brief Allocates memory for a string of [len] characters, setting the first
+ * and len character to the null-byte.
+ *
+ * @param len (size_t): number of character to allocate for.
+ * @return (t_cstr): null-terminated string capable of holding [len] characters.
+ */
 t_cstr	cstr_alloc(size_t len)
 {
 	t_cstr	cstr;
 
 	cstr = safe_alloc(len + 1);
 	*cstr = '\0';
+	cstr[len] = '\0';
 
 	return (cstr);
 }
