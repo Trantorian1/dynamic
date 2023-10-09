3066ebb81f8e39b50d13659c6363d5ff43d4e90f
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-09 11:43:26 +0000

    test(str): added tests for cstr_find_cstr

--- src/cstr/cstr_find_cstr.c
+++ src/cstr/cstr_find_cstr.c
@@ -6,16 +6,18 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 17:34:07 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/09 09:58:16 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/09 11:41:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cstr_find_cstr.h"
 
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "cstr_len.h"
+#include "dyn_memcmp.h"
 
 size_t	cstr_find_cstr(t_cstr _Nonnull cstr_a, t_cstr _Nonnull cstr_b)
 {
@@ -29,18 +31,18 @@ size_t	cstr_find_cstr(t_cstr _Nonnull cstr_a, t_cstr _Nonnull cstr_b)
 	len_a = cstr_len(cstr_a);
 	len_b = cstr_len(cstr_b);
 
-	if (len_b > len_a)
+	if (len_b > len_a || len_b == 0)
 		return (len_a);
 
 	index = 0;
 	while (len_a - index - len_b > 0)
 	{
-		if (memcpy(cstr_a + index, cstr_b, len_b) == 0)
+		if (dyn_memcmp(cstr_a + index, cstr_b, len_b) == 0)
 			return (index);
 		index++;
 	}
 
-	if (memcpy(cstr_a + index, cstr_b, len_b) == 0)
+	if (dyn_memcmp(cstr_a + index, cstr_b, len_b) == 0)
 		return (index);
 
 	return (len_a);
