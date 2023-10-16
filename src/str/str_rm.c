a576c66c6331e909ceadeb5c76c40bf5af2b7eb5
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-16 16:18:33 +0000

    feat(str): added and tested function to remove segments of a string

---
+++ src/str/str_rm.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_rm.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 16:03:26 by marvin            #+#    #+#             */
+/*   Updated: 2023/10/16 16:18:14 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "str_rm.h"
+
+#include "dyn_memove.h"
+
+void	str_rm(t_str *_Nonnull str, size_t start, size_t stop)
+{
+	if (str == NULL || start > stop || start > str->len || start == stop)
+		return ;
+	dyn_memove(str->get + start, str->get + stop, str->len - stop + 1);
+}
