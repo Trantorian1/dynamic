c0fc5280fa22882f6f79cac74d6db3275b6476c1
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-16 07:39:09 +0000

    feat(header): updated library headers

--- string.h
+++ string.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:36:34 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/06 17:17:05 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/16 07:29:35 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 #  define STR_LEN_MIN 16
 # endif
 
+# include <stdbool.h>
 # include "struct.h"
 
+t_str 				str_alloc(size_t len);
 t_str *_Nullable	str_create(t_cstr _Nonnull cstr);
 void				str_destroy(t_str *_Nullable str);
 t_str *_Nullable	str_append_char(t_str *_Nonnull str, char c);
@@ -29,9 +31,12 @@ t_str *_Nullable	str_insert_char(t_str *_Nonnull str, char c, size_t index);
 t_str *_Nullable	str_insert_str(t_str *_Nonnull str, t_cstr _Nonnull cstr, size_t index);
 t_str *_Nullable	str_substr(t_str *_Nonnull str, size_t start, size_t stop);
 size_t				str_find_char(t_str *_Nonnull str, char c);
+t_vptr *_Nullable	str_split(t_str str, t_cstr _Nonnull pattern);
 
 t_cstr _Nullable	cstr_alloc(size_t len);
 t_cstr _Nullable	cstr_dup(t_cstr _Nonnull cstr);
 size_t				cstr_len(t_cstr _Nonnull cstr);
+size_t				cstr_find_cstr(t_cstr _Nonnull cstr_a, t_cstr _Nonnull cstr_b);
+bool				cstr_eq(t_cstr _Nonnull cstr_a, t_cstr _Nonnull cstr_b);
 
 #endif
---
+++ vector.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 07:30:53 by marvin            #+#    #+#             */
+/*   Updated: 2023/10/16 07:38:41 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_H
+# define VECTOR_H
+
+# ifndef VEC_PTR_LEN_MIN
+#  define VEC_PTR_LEN_MIN 16
+# endif
+
+# include "struct.h"
+
+# define vptr_create(TYPE, n) vptr_create_impl(sizeof(TYPE), n)
+# define vptr_collect(type, vptr) ((type *) vptr_collect_impl(vptr))
+
+t_vptr *_Nonnull	vptr_create_impl(size_t elem_size, size_t n);
+void				vptr_destroy(t_vptr *_Nullable vptr);
+t_vptr *_Nullable	vptr_append(t_vptr *_Nonnull vptr, void *_Nullable val);
+t_vptr *_Nullable	vptr_insert(t_vptr *_Nonnull vptr, void *_Nonnull data, size_t index);
+void *_Nullable		vptr_collect_impl(t_vptr *_Nonnull vptr);
+
+t_vptr *_Nullable	vstr_append(t_vptr *_Nonnull vstr, t_str str);
+void				vstr_destroy(t_vptr *_Nonnull vstr);
+t_vptr *_Nullable	vstr_insert(t_vptr *_Nonnull vstr, t_str str, size_t index);
+
+#endif
