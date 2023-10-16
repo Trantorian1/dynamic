5deb39d7e6e6f894b487b288c678b9ac3c35a690
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-16 17:07:25 +0000

    fix(vec): vptr_rm now returns a pointer to the vector being mutated

--- include/vptr_rm.h
+++ include/vptr_rm.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:45:55 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/16 16:56:18 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/16 17:06:35 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <stddef.h>
 # include "s_vptr.h"
 
-void	vptr_rm(t_vptr *_Nonnull vptr, size_t index);
+t_vptr *_Nullable	vptr_rm(t_vptr *_Nonnull vptr, size_t index);
 
 #endif
--- src/vec/ptr/vptr_rm.c
+++ src/vec/ptr/vptr_rm.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:46:49 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/16 16:59:27 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/16 17:07:03 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 #include "dyn_memove.h"
 
-void	vptr_rm(t_vptr *_Nonnull vptr, size_t index)
+t_vptr *_Nullable	vptr_rm(t_vptr *_Nonnull vptr, size_t index)
 {
 	char	*target;
 	size_t	bytes;
 
 	if (vptr == NULL || index >= vptr->len)
-		return ;
+		return (NULL);
 
 	target = (char *)vptr->data + index * vptr->_elem_size;
 	bytes = (vptr->len - index) * vptr->_elem_size;
 	dyn_memove(target, target + vptr->_elem_size, bytes);
 
 	vptr->len--;
+	return (vptr);
 }
