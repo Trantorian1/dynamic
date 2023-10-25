7e5814f6801b814ae08181ff8e4f13e2b678f65b
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-25 00:55:30 +0000

    feat(vec): added and tested vstr_collect
    Collects an array of dynamic string to a 2D array of C strings

--- include/vstr_collect.h
+++ include/vstr_collect.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 14:10:48 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/10 14:12:33 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/25 00:40:31 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include "s_vptr.h"
 # include "s_str.h"
 
-t_str *_Nonnull *_Nullable	vstr_collect(t_vptr *_Nonnull vstr);
+t_cstr _Nonnull *_Nullable	vstr_collect(t_vptr *_Nonnull vstr);
 
 #endif
